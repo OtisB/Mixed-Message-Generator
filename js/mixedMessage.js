628512cb3798de125d3193c4b949bc34018ef150
Robin Schmidt <Robin Schmidt>
2022-08-08 11:37:54 +0200

    removed comment

--- js/mixedMessage.js
+++ js/mixedMessage.js
@@ -2,7 +2,31 @@
 
 console.clear();
 
-// const randomNumber = Math.random(3);
 
-console.log('Hello there, General Kenobi!');
-// console.log(randomNumber);
+const verbs = ['Do', 'Get', 'Walk', 'Run', 'Leave', 'Buy', 'Set'];
+const adj = ['crazy', 'unnecessary', 'stupid', 'big', 'small', 'great'];
+const nouns = ['stuff', 'races', 'goals', 'steps'];
+const punctuation = ['...', '?', '.', '!'];
+
+const generateMessage = () => {
+  if (verbs.length > 0 && adj.length > 0 &&
+    nouns.length > 0 && punctuation.length > 0) {
+
+    const mixedMessage = []
+    mixedMessage.push(
+      verbs[Math.floor(Math.random() * verbs.length)],      //[0].toUpperCase(), -->Versuch den ersten Buchstaben groß zumachen
+      adj[Math.floor(Math.random() * adj.length)],
+      nouns[Math.floor(Math.random() * nouns.length)],
+      punctuation[Math.floor(Math.random() * punctuation.length)]
+    );
+    console.log(mixedMessage.join(' '));
+  }
+  else
+    console.log('Done is better than perfect...');
+};
+
+generateMessage(); //logs array
+
+
+
+
