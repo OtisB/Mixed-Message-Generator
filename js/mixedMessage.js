0b4a15bd82d57483425cf220938bb4fb523e71b9
Robin Schmidt <Robin Schmidt>
2022-08-08 10:16:55 +0200

    inital commit

---
+++ js/mixedMessage.js
@@ -0,0 +1,8 @@
+// mixed message, created: 08.08.2022, Robin Schmidt
+
+console.clear();
+
+// const randomNumber = Math.random(3);
+
+console.log('Hello there, General Kenobi!');
+// console.log(randomNumber);
